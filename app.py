4a7b96552fb8b33bf57ad99c09d3b4486c08b8f7
Aditya Sahu <adityasahu1511@gmail.com>
2024-10-05 22:31:46 -0400

    update port

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ if __name__ == '__main__':
     CORS(app)
 
     # Run the app with debug mode to see logs
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    #app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run()
